a7b297735b6e4a7d6f13bdc57025598bbde5dd89
leonardoserra <serra.leonardo92@gmail.com>
2023-05-30 15:56:59 +0200

    chiamata get con axios ok

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <script>
-import TemplateComponent from './components/TemplateComponent.vue';
-
+import ProjectCard from './components/ProjectCard.vue';
+import axios from 'axios';
 export default {
+  data() {
+    return {
+      baseURL: 'http://127.0.0.1:8000'
+    }
+  },
   components: {
-    TemplateComponent
+    ProjectCard
+  },
+  methods: {
+    getProjects() {
+      axios.get(`${this.baseURL}/api/projects`)
+        .then(response =>
+          console.log(response.data.results)
+        )
+
+    }
+  },
+  mounted() {
+    this.getProjects();
   }
 }
 </script>
 
 <template>
-  <TemplateComponent></TemplateComponent>
+  <ProjectCard></ProjectCard>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import './styles/general.scss'
+</style>
