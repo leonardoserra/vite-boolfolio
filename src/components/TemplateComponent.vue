1ebd355b1fa276668c63cdbda4d72567972ea506
leonardoserra <serra.leonardo92@gmail.com>
2023-05-30 15:31:37 +0200

    creato un componente template di prova

---
+++ src/components/TemplateComponent.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+            messaggioProva: 'ciao carissimi'
+        }
+    }
+}
+</script>
+
+<template>
+    {{ messaggioProva }}
+</template>
+
+<style scoped lang="scss"></style>
