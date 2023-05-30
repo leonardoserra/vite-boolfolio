d99b2d11b5c1ca7bd6681d10480e58b0fa581330
leonardoserra <serra.leonardo92@gmail.com>
2023-05-30 17:09:45 +0200

    passo le props ma non ce niente..

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
   methods: {
     getProjects(dataToFill) {
       axios.get(`${this.baseURL}/api/projects`)
-        .then(function (response) {
+        .then(response => {
           console.log(response.data.results);
           dataToFill = response.data.results;
           console.log(dataToFill);
@@ -31,9 +31,36 @@ export default {
 </script>
 
 <template>
-  <ProjectCard :projects="projects"></ProjectCard>
+  <div class="card-container">
+
+    <div v-for="(project, index) in projects" :key="index" class="card">
+      <ProjectCard :title="project.title" :imageSrc="project.image_src" :description="project.description"
+        :technologies="project.technologies" :type="project.type?.type_name"></ProjectCard>
+    </div>
+
+
+  </div>
 </template>
 
 <style scoped lang="scss">
-@import './styles/general.scss'
+@import './styles/general.scss';
+
+.card-container {
+  width: 1200px;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
+
+  .card {
+    margin: 10px;
+    width: calc(33.3333333333% - 54px);
+    border: 2px solid rgb(253, 66, 66);
+    border-radius: 12px;
+    padding: 15px;
+    box-shadow: 3px 3px rgb(253, 66, 66);
+  }
+
+}
 </style>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -7,7 +7,11 @@ export default {
         }
     },
     props: {
-        projects: Array
+        title: String,
+        imageSrc: String,
+        description: String,
+        technologies: Array,
+        type: String
     },
     mounted() {
         console.log(this.projects);
@@ -16,39 +20,36 @@ export default {
 </script>
 
 <template>
-    <!-- <div class="prova">
-        {{ messaggioProva }}
-    </div> -->
-
-    <div class="card-container">
-
-        <div v-for="(project, index) in this.projects" :key="index" class="card">
-            {{ project.title }}
-        </div>
-
-
-    </div>
+    <ul>
+        <li>{{ title }}</li>
+        <li>{{ imageSrc }}</li>
+        <li>{{ description }}</li>
+        <li v-for="technology in technologies">
+            <div>{{ technology.name }}</div>
+        </li>
+        <li>{{ type }}</li>
+    </ul>
 </template>
 
 <style scoped lang="scss">
 @import '../styles/general.scss';
 
-.card-container {
-    width: 1200px;
-    margin: auto;
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
+// .card-container {
+//     width: 1200px;
+//     margin: auto;
+//     display: flex;
+//     justify-content: center;
+//     flex-wrap: wrap;
 
 
-    .card {
-        margin: 10px;
-        width: calc(33.3333333333% - 54px);
-        border: 2px solid rgb(253, 66, 66);
-        border-radius: 12px;
-        padding: 15px;
-        box-shadow: 3px 3px rgb(253, 66, 66);
-    }
+//     .card {
+//         margin: 10px;
+//         width: calc(33.3333333333% - 54px);
+//         border: 2px solid rgb(253, 66, 66);
+//         border-radius: 12px;
+//         padding: 15px;
+//         box-shadow: 3px 3px rgb(253, 66, 66);
+//     }
 
-}
+// }
 </style>
