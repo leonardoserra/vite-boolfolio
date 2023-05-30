69cbf9e5d61773a64c460852ff9659e7baa92fd8
leonardoserra <serra.leonardo92@gmail.com>
2023-05-30 18:19:06 +0200

    stile migliorato

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -20,27 +20,38 @@ export default {
 </script>
 
 <template>
-    <div class="card">
+    <div class="card m-4">
         <img :src="imageSrc" class="card-img-top img-thumbnail" :alt="title">
         <div class="card-body">
-            <h5 class="card-title">TITOLO: {{ title }}</h5>
-            <p class="card-text">DESCRIZIONE: {{ description }}</p>
+            <h5 class="card-title">Titolo: {{ title }}</h5>
+            <p class="card-text">Descrizione: {{ description }}</p>
             <div class="row">
                 <div class="col">
-                    TECNOLOGIE: <ul class="list-group-flush">
-                        <li v-for="technology in technologies" class="list-group-item">{{ technology.name }}
+                    Tecnologie: <ul class="list-group-flush">
+                        <li v-for="technology in technologies" class="list-group-item">
+                            {{ technology.name }}
                         </li>
 
                     </ul>
                 </div>
             </div>
-            <div>
-                TIPOLOGIA: {{ type }}
+            <div v-if="type">
+                Tipologia: {{ type }}
             </div>
+            <div v-else>Tipologia: Nessuna tipologia assegnata</div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
 @import '../styles/general.scss';
+
+.card {
+    min-height: 650px;
+
+
+    img {
+        max-height: 400px;
+    }
+}
 </style>
