becca8f441b0d1c955fb3c646eebd19091a656e2
leonardoserra <serra.leonardo92@gmail.com>
2023-05-31 21:30:48 +0200

    creata card show per singolo project

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -16,9 +16,10 @@ export default {
 
         console.log(slug);
 
-        axios.get(`${this.store.baseURL}/api/projects/${slug}`)
+        axios.get(`${this.store.baseURL}/api/project/${slug}`)
             .then(response => {
                 this.singleProject = response.data.results;
+                console.log(this.singleProject);
             });
 
     }
@@ -26,7 +27,26 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <div class="card col-6 mt-5">
+        <img :src="singleProject.image_src" class="card-img-top" :alt="singleProject.title">
+        <div class="card-body">
+            <h5 class="card-title">{{ singleProject.title }}</h5>
+            <p class="card-text">{{ singleProject.description }}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                <div v-for="(technology, index) in singleProject.technologies" :key="index"> {{ technology.name }} </div>
+            </li>
+            <li class="list-group-item">{{ singleProject.type?.type_name }}</li>
+        </ul>
+        <span class="btn btn-success">
+
+            <router-link :to="{ name: 'projects' }" class="nav-link ">Torna alla lista di
+                progetti</router-link>
+        </span>
+    </div>
+</template>
 
 <style lang="scss" >
 @import '../styles/general.scss';
