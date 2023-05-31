263dfdc68c9f638eda54ca3e9b94f2561146b32b
leonardoserra <serra.leonardo92@gmail.com>
2023-05-31 19:53:23 +0200

    creata rotta per SingleProject, axios call  slug

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -16,25 +16,28 @@ export default {
 </script>
 
 <template>
-    <div class="card m-4 col-3 " v-for="(project, index) in projects" :key="index">
+    <div class="card m-4 col-3   " v-for="(project, index) in projects" :key="index">
         <img :src="project.image_src" class="card-img-top img-thumbnail" :alt="project.title">
         <div class="card-body">
-            <h5 class="card-title">Titolo: {{ project.title }}</h5>
-            <p class="card-text">Descrizione: {{ project.description }}</p>
+            <h5 class="card-title mt-2 ">Titolo: {{ project.title }}</h5>
+            <p class="card-text mt-2 border-bottom">Descrizione: {{ project.description }}</p>
             <div class="row">
                 <div class="col">
-                    Tecnologie: <ul v-if="project.technologies.length >= 1" class="card-text list-group-flush">
-                        <li v-for="technology in project.technologies" class="list-group-item">
+                    Tecnologie: <ul v-if="project.technologies.length >= 1" class="card-text mt-2 list-group-flush">
+                        <li v-for="technology in project.technologies" class="list-group-item  mt-2  border-bottom">
                             {{ technology.name }}
                         </li>
                     </ul>
-                    <span class="card-text" v-else>Nessuna Tecnologia usata</span>
+                    <span class="card-text mt-2" v-else>Nessuna Tecnologia usata</span>
                 </div>
             </div>
-            <div class="card-text" v-if="project.type">
+            <div class="card-text mt-2" v-if="project.type">
                 Tipologia: {{ project.type.type_name }}
             </div>
-            <div class="card-text" v-else>Tipologia: Nessuna tipologia assegnata</div>
+            <div class="card-text mt-2" v-else>Tipologia: Nessuna tipologia assegnata</div>
+            <router-link :to="{ name: 'single-project', params: { slug: project.slug } }" class="btn btn-primary">Dettagli
+                singolo
+                Project</router-link>
         </div>
     </div>
 </template>
---
+++ src/pages/SingleProject.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from 'axios';
+import { store } from '../store.js';
+
+export default {
+    name: 'SingleProject',
+    data() {
+        return {
+            store,
+            singleProject: []
+
+        }
+    },
+    mounted() {
+        const slug = this.$route.params.slug;
+
+        console.log(slug);
+
+        axios.get(`${this.store.baseURL}/api/projects/${slug}`)
+            .then(response => {
+                this.singleProject = response.data.results;
+            });
+
+    }
+}
+
+</script>
+
+<template></template>
+
+<style lang="scss" >
+@import '../styles/general.scss';
+</style>
