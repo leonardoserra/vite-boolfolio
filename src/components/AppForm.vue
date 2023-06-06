89fccc0e02ebde07a476d02a379ffd9663eaf1ec
leonardoserra <serra.leonardo92@gmail.com>
2023-06-06 17:38:00 +0200

    aggiungo form

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -3,7 +3,9 @@ export default {
     name: 'AppForm',
     data() {
         return {
-            messaggioProva: 'ciao carissimi'
+            name: '',
+            email: '',
+            message: ''
         }
     }
 }
@@ -12,13 +14,20 @@ export default {
 <template>
     <form class="col">
         <div class="mb-3">
+            <label for="name" class="form-label">Inserisci in tuo nome</label>
+            <input type="text" class="form-control" id="name" v-model="name">
+        </div>
+
+        <div class="mb-3">
             <label for="email" class="form-label">Inserisci la tua e-mail</label>
-            <input type="email" class="form-control" id="email">
+            <input type="email" class="form-control" id="email" v-model="email">
             <div id="emailHelp" class="form-text">Usa la mail con la quale desideri essere ricontattato</div>
         </div>
         <div class="mb-3">
-            <label for="name" class="form-label">Inserisci in tuo nome</label>
-            <input type="text" class="form-control" id="name">
+            <label for="message" class="mb-2">Messaggio</label>
+            <textarea class="form-control" placeholder="Inserisci il tuo messaggio (facoltativo..)" id="message"
+                v-model="message"></textarea>
+
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
